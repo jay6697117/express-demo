6fa43be691572c80b8b56eeb87deb351dd775bbb
SteveZhang <871506263@qq.com>
2024-08-14 00:49:47 +0800

    1

--- router/index.js
+++ router/index.js
@@ -1,10 +1,11 @@
 import method from './routes/method.js';
 import demo from './routes/demo.js';
 import header from './routes/header.js';
+import upload from './routes/upload.js';
 import restful from './routes/restful.js';
 
 // 路由列表
-const routers = [demo, header];
+const routers = [demo, header, upload];
 
 export default function router(app) {
   // 挂载方法
